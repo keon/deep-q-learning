0b3cece3e52b208d649e08f42565de15fb552220
Ken Fehling <kenfehling@gmail.com>
2017-07-09 17:34:49 -0400

    Fixed indentation

--- dqn.py
+++ dqn.py
@@ -46,7 +46,7 @@ class DQNAgent:
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-              target = reward + self.gamma * \
+                target = reward + self.gamma * \
                        np.amax(self.model.predict(next_state)[0])
             target_f = self.model.predict(state)
             target_f[0][action] = target
